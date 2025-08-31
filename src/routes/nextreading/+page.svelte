7bfa990af04346976354ac02b5072ebd7f1729fb
Zachary Luettgen <zluettgen@gmail.com>
2025-08-31 14:02:32 -0400

    add next reading route and page

---
+++ src/routes/nextreading/+page.svelte
@@ -0,0 +1,19 @@
+<div class="font-[Commissioner] text-xl w-1/2">
+  <h2 class="p-4 text-2xl font-bold">date</h2>
+  <p class="px-6 py-2">Friday, September 19th, 2025</p>
+  <h2 class="p-4 text-2xl font-bold">place</h2>
+  <p class="px-6 py-2">20 Love Pl., Pittsburgh, Pennsylvania</p>
+  <h2 class="p-4 text-2xl font-bold">program</h2>
+  <ul class="px-6 py-2 list-disc list-inside">
+    <li class="py-1">opening party, 6p to 8p</li>
+    <li class="py-1">the reading, 8p to 9p, featuring...</li>
+    <ul class="px-6 py-2 list-disc list-inside">
+      <li class="py-1">Noushin</li>
+      <li class="py-1">Tamar</li>
+      <li class="py-1">Tai</li>
+      <li class="py-1">Alexa</li>
+    </ul>
+    <li class="py-1">closing party, 9p to late</li>
+  </ul>
+  <p class="p-4">We hope to see you there.</p>
+</div>
