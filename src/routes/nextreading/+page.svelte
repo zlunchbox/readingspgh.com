b064654c0e549c7eb62ecf0772414e03ce4d456e
Zachary Luettgen <zluettgen@gmail.com>
2025-09-05 20:55:06 -0400

    adjust base font size, adjust next reading fonts and layout

--- src/routes/nextreading/+page.svelte
+++ src/routes/nextreading/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-  import Triangles from "$lib/components/DynamicShape/Triangles.svelte";
 </script>
 
-<div class="font-[Archivo] text-xl w-1/2">
+<div class="font-[Archivo] text-xl">
   <h2 class="p-4 text-2xl font-bold">time</h2>
-  <p class="px-6 py-2">Friday, September 19th, 2025</p>
+  <p class="px-8 py-2">Friday, September 19th, 2025</p>
   <h2 class="p-4 text-2xl font-bold">place</h2>
-  <p class="px-6 py-2">20 Love Pl., Pittsburgh, Pennsylvania</p>
+  <p class="px-8 py-2">20 Love Pl.,<br /> Pittsburgh, Pennsylvania</p>
   <h2 class="p-4 text-2xl font-bold">program</h2>
-  <p class="px-6 py-2">
+  <p class="px-8 py-2">
     opening party
-    <span class="text-base">6p to 8p</span>
+    <span class="block text-base">6p to 8p</span>
   </p>
-  <p class="px-6 py-2">
+  <p class="px-8 py-2">
     the reading
-    <span class="text-base">8p to 9p</span>
+    <span class="block text-base">8p to 9p</span>
   </p>
   <ul class="px-12 py-2 list-disc list-inside">
     <li class="py-1">Noushin</li>
@@ -22,9 +21,9 @@
     <li class="py-1">Tai</li>
     <li class="py-1">Alexa</li>
   </ul>
-  <p class="px-6 py-2">
+  <p class="px-8 py-2">
     closing party
-    <span class="text-base">9p to late</span>
+    <span class="block text-base">9p to late</span>
   </p>
   <p class="p-4">See you there.</p>
 </div>
