0762fff990513eecfc8531d65ece99b9cda63fb7
Zachary Luettgen <zluettgen@gmail.com>
2025-09-05 20:41:21 -0400

    responsive adjustments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,12 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center h-screen">
+<div class="flex flex-col items-center justify-center min-h-full">
   <div class="w-full flex-4 flex flex-col items-center justify-center gap-10">
     {@render children?.()}
   </div>
   <nav
-    class="w-1/2 font-[Archivo] flex-1 flex flex-col lg:flex-row items-center justify-center text-xl"
+    class="w-full md:w-1/2 font-[Archivo] flex-1 flex flex-row items-center justify-center text-xl"
   >
     <div class="flex-1 text-center">
       <a class="hover:font-bold" href="/">home</a>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 </script>
 
-<div class="font-[Archivo] text-xl lg:w-1/2 z-10">
-  <p class="p-4">
+<div class="font-[Archivo] text-xl md:w-1/2 z-10">
+  <p class="px-6 py-4">
     The Readings are a biannual literary reading series—the Spring and Fall
     Reading.
   </p>
-  <p class="p-4">
+  <p class="px-6 py-4">
     Each Reading presents three to five writers, their work, and a space for
     everyone to commune on art, culture, and life.
   </p>
-  <p class="p-4">
+  <p class="px-6 py-4">
     The Readings are free and informal. Anyone is welcome to read, at any skill
     level, of any form, from any genre. Writers are invited to participate at a
     Reading, it is not an open mic.
   </p>
-  <p class="p-4">The program goes like this:</p>
-  <ul class="px-6 py-2 list-disc list-inside">
+  <p class="px-6 py-4">The program goes like this:</p>
+  <ul class="px-10 py-2 list-disc list-inside">
     <li class="py-2">
       The opening party, when people arrive, mingle, meet, munch, talk, and
       laugh.
@@ -27,5 +27,5 @@
       each other better.
     </li>
   </ul>
-  <p class="p-4">We hope to see you soon.</p>
+  <p class="px-6 py-4">We hope to see you soon.</p>
 </div>
