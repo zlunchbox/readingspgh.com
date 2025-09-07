692cb31d07bd326098e118ebb1c64533b8fbeb71
Zachary Luettgen <zluettgen@gmail.com>
2025-09-06 22:54:33 -0400

    update padding

--- src/routes/nextreading/+page.svelte
+++ src/routes/nextreading/+page.svelte
@@ -3,25 +3,25 @@
 
 <div class="font-[Archivo] text-xl">
   <h2 class="p-4 text-2xl font-bold">time</h2>
-  <p class="px-8 py-2">Friday, September 19th, 2025</p>
+  <p class="px-8">Friday, September 19th, 2025</p>
   <h2 class="p-4 text-2xl font-bold">place</h2>
-  <p class="px-8 py-2">20 Love Pl.,<br /> Pittsburgh, Pennsylvania</p>
+  <p class="px-8">20 Love Pl.,<br /> Pittsburgh, Pennsylvania</p>
   <h2 class="p-4 text-2xl font-bold">program</h2>
-  <p class="px-8 py-2">
+  <p class="px-8">
     opening party
     <span class="block text-base">6p to 8p</span>
   </p>
-  <p class="px-8 py-2">
+  <p class="px-8 pt-2">
     the reading
     <span class="block text-base">8p to 9p</span>
   </p>
-  <ul class="px-12 py-2 list-disc list-inside">
-    <li class="py-1">Noushin</li>
-    <li class="py-1">Tamar</li>
-    <li class="py-1">Tai</li>
-    <li class="py-1">Alexa</li>
+  <ul class="px-12 py-1 list-disc list-inside">
+    <li class="">Noushin</li>
+    <li class="">Tamar</li>
+    <li class="">Tai</li>
+    <li class="">Alexa</li>
   </ul>
-  <p class="px-8 py-2">
+  <p class="px-8 pt-1">
     closing party
     <span class="block text-base">9p to late</span>
   </p>
