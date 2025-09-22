4bb6decc84689a723ae930e5582f2aa9ffec0c9d
Zachary Luettgen <zluettgen@gmail.com>
2025-09-21 20:19:00 -0400

    no unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
   import Subtitle from "$lib/components/Subtitle/Subtitle.svelte";
-  import BannerInfo from "$lib/components/BannerInfo/BannerInfo.svelte";
 </script>
 
 <h1 class="font-semibold text-center text-5xl lg:text-8xl flex-0">
