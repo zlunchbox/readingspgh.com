75d90abdc4204a7ed0c75085e5d24a5ad4194f60
Zachary Luettgen <zluettgen@gmail.com>
2025-09-21 20:22:56 -0400

    update "next reading"

--- src/routes/nextreading/+page.svelte
+++ src/routes/nextreading/+page.svelte
@@ -2,39 +2,6 @@
 </script>
 
 <div class="text-xl">
-  <h2 class="p-4 text-2xl font-bold">time</h2>
-  <p class="px-8">Friday, September 19th, 2025</p>
-  <h2 class="p-4 text-2xl font-bold">place</h2>
-  <p class="px-8">20 Love Pl.,<br /> Pittsburgh, Pennsylvania</p>
-  <h2 class="p-4 text-2xl font-bold">program</h2>
-  <p class="px-8">
-    opening party
-    <span class="block text-base">6p to 8p</span>
-  </p>
-  <p class="px-8 pt-2">
-    the reading
-    <span class="block text-base">8p to 9p</span>
-  </p>
-  <ul class="px-12 py-1 list-disc list-inside">
-    <li class="text-lg">
-      <a class="transition hover:text-gray-500" href="/readers/noushin">
-        Noushin Quazi
-      </a>
-    </li>
-    <li class="text-lg">
-      <a class="transition hover:text-gray-500" href="/readers/tamar">
-        Tamar Tipton
-      </a>
-    </li>
-    <li class="text-lg">
-      <a class="transition hover:text-gray-500" href="/readers/alexa">
-        Alexa Bocek
-      </a>
-    </li>
-  </ul>
-  <p class="px-8 pt-1">
-    closing party
-    <span class="block text-base">9p to late</span>
-  </p>
-  <p class="p-4">See you there.</p>
+  <h2 class="p-4 text-2xl font-bold">stay tuned</h2>
+  <p class="p-4">Third Spring Reading announcements coming in 2026</p>
 </div>
