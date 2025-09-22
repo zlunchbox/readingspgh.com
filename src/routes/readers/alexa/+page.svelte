d47ff0801ae1601464e96d4b41640450d53f45df
Zachary Luettgen <zluettgen@gmail.com>
2025-09-21 20:10:37 -0400

    add bio pages for tamar and alexa

---
+++ src/routes/readers/alexa/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Biography from "$lib/components/Biography/Biography.svelte";
+</script>
+
+<Biography name="Alexa Bocek">
+  <p class="px-5 py-2">
+    Alexa is a writer from Pittsburgh whose poetry centers around the aching
+    opposites of life; the grey areas where two conflicting things can both be
+    true.
+  </p>
+  <p class="px-5 py-2">
+    As a bisexual creative who co-hosts community art events called Trash
+    Crafts, writes a blog and makes multi-media art for her project In the Nude
+    Ceramics, and in her full-time work teaches creative writing and audio
+    production to teens, Alexa wants to use this one little blink of life to
+    connect with others and to learn. She wants to thank you for having her.
+  </p>
+</Biography>
---
+++ src/routes/readers/tamar/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Biography from "$lib/components/Biography/Biography.svelte";
+</script>
+
+<Biography name="Tamar Tipton">
+  <p class="px-5 py-2">
+    Tamar is a non-binary speculative fiction writer and mosaic artist. They are
+    the current City Books Writer in Residence. They love makeup, mukbangs, and
+    their cat Shiori.
+  </p>
+</Biography>
