45076469ef5fbff3517fb47e27837f02abf763bb
Zachary Luettgen <zluettgen@gmail.com>
2025-09-21 20:11:18 -0400

    adjust padding of biographies

--- src/lib/components/Biography/Biography.svelte
+++ src/lib/components/Biography/Biography.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="md:w-1/2">
-  <h1 class="text-xl lg:text-2xl pb-5 bold">{name}</h1>
+  <h1 class="text-xl lg:text-2xl px-5 pt-5 pb-2 bold">{name}</h1>
   <div class="text-lg">
     {@render children()}
   </div>
