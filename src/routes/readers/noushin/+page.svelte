89dafe79e8ea43d592e616c7c1682e266dd45b04
Zachary Luettgen <zluettgen@gmail.com>
2025-09-21 20:10:49 -0400

    adjust padding

--- src/routes/readers/noushin/+page.svelte
+++ src/routes/readers/noushin/+page.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <Biography name="Noushin Quazi">
-  <p class="pb-5">
+  <p class="px-5 py-2">
     A failed artist who tries to paint with words now. From rhyming random words
     as a party trick to amuse guests during her childhood, to rhyming
     less-random words and mostly not even attempting to rhyme at all, Noushin is
     proud to say she has come a long way in her journey as a writer and
     story-teller.
   </p>
-  <p>
+  <p class="px-5 py-2">
     Noushin is working towards her PhD in neuroscience, and often finds herself
     grappling with the double-edged sword of knowledge: that while knowledge is
     power, it can also be the diminishment of what precious magic remains in
